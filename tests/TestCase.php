3bb6fc590e3e132701cbe72c8881d009237e951c
Marco Rieser <marco@rieser.dev>
2025-05-09 08:10:53 +0200

    Add spatie/laravel-ray as dev dependency

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace MarcoRieser\Livewire\Tests;
 
 use Livewire\LivewireServiceProvider;
 use MarcoRieser\Livewire\ServiceProvider;
+use Spatie\LaravelRay\RayServiceProvider;
 use Statamic\Testing\AddonTestCase;
 
 abstract class TestCase extends AddonTestCase
@@ -13,7 +14,10 @@ abstract class TestCase extends AddonTestCase
     protected function getPackageProviders($app): array
     {
         return array_merge(
-            [LivewireServiceProvider::class],
+            [
+                LivewireServiceProvider::class,
+                RayServiceProvider::class,
+            ],
             parent::getPackageProviders($app)
         );
     }
