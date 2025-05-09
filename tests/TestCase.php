b62642743cb047ee6c330e4fe0c25c7c04d1c257
Marco Rieser <marco@rieser.dev>
2025-05-09 12:58:14 +0200

    Setup test suite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,10 @@
 
 namespace MarcoRieser\Livewire\Tests;
 
+use Illuminate\Support\Arr;
 use Livewire\LivewireServiceProvider;
 use MarcoRieser\Livewire\ServiceProvider;
+use Spatie\LaravelRay\RayServiceProvider;
 use Statamic\Testing\AddonTestCase;
 
 abstract class TestCase extends AddonTestCase
@@ -13,8 +15,20 @@ abstract class TestCase extends AddonTestCase
     protected function getPackageProviders($app): array
     {
         return array_merge(
-            [LivewireServiceProvider::class],
+            [
+                LivewireServiceProvider::class,
+                RayServiceProvider::class,
+            ],
             parent::getPackageProviders($app)
         );
+    }
+
+    protected function setConfigValue(string $key, $value): void
+    {
+        $config = config()->array('statamic-livewire', require __DIR__.'/../config/statamic-livewire.php');
+
+        Arr::set($config, $key, $value);
+
+        config()->set('statamic-livewire', $config);
     }
 }
