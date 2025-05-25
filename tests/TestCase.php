f34fda9b4485881b5f2d47bfedbb861b058be360
Marco Rieser <marco@rieser.dev>
2025-05-25 10:45:01 +0200

    Add config manipulation helpers as trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace MarcoRieser\Livewire\Tests;
 
-use Illuminate\Support\Arr;
 use Livewire\LivewireServiceProvider;
 use MarcoRieser\Livewire\ServiceProvider;
 use Spatie\LaravelRay\RayServiceProvider;
@@ -23,15 +22,6 @@ abstract class TestCase extends AddonTestCase
             ],
             parent::getPackageProviders($app)
         );
-    }
-
-    protected function setConfigValue(string $key, $value): void
-    {
-        $config = config()->array('statamic-livewire', require __DIR__.'/../config/statamic-livewire.php');
-
-        Arr::set($config, $key, $value);
-
-        config()->set('statamic-livewire', $config);
     }
 
     protected function setUp(): void
