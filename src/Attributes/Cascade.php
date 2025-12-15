c00cd16c4946df07bdf95f9bd8f316ba0fec885f
Marco Rieser <marco@rieser.dev>
2025-12-15 17:16:50 +0100

    Extract methods

--- src/Attributes/Cascade.php
+++ src/Attributes/Cascade.php
@@ -3,10 +3,12 @@
 namespace MarcoRieser\Livewire\Attributes;
 
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Str;
 use Livewire\Features\SupportAttributes\Attribute as LivewireAttribute;
 use Livewire\Livewire;
 use Statamic\Exceptions\CascadeDataNotFoundException;
 use Statamic\Facades\Cascade as CascadeFacade;
+use Statamic\Facades\Entry;
 use Statamic\Facades\Site;
 use Statamic\Support\Arr;
 
@@ -14,19 +16,21 @@ use Statamic\Support\Arr;
 class Cascade extends LivewireAttribute
 {
     public function __construct(
-        public $keys = null,
+        public array $keys = [],
+        public bool $content = true,
     ) {}
 
     public function getCascadeData(): array
     {
         if (! $data = CascadeFacade::toArray()) {
-            CascadeFacade::withSite(Site::current());
-            CascadeFacade::withRequest(Request::create(uri: Livewire::originalUrl(), method: Livewire::originalMethod()));
+            $this->hydrateSite();
+            $this->hydrateRequest();
+            $this->hydrateContent();
 
             $data = CascadeFacade::hydrate()->toArray();
         }
 
-        if (! isset($this->keys)) {
+        if (! $this->keys) {
             return $data;
         }
 
@@ -35,6 +39,7 @@ class Cascade extends LivewireAttribute
                 if (is_numeric($key)) {
                     $key = $default;
                     $default = null;
+
                     if (! array_key_exists($key, $data)) {
                         throw new CascadeDataNotFoundException($key);
                     }
@@ -43,5 +48,32 @@ class Cascade extends LivewireAttribute
                 return [$key => Arr::get($data, $key, $default)];
             })
             ->all();
+    }
+
+    protected function hydrateSite(): void
+    {
+        CascadeFacade::withSite(Site::current());
+    }
+
+    protected function hydrateRequest(): void
+    {
+        CascadeFacade::withRequest(Request::create(uri: Livewire::originalUrl(), method: Livewire::originalMethod()));
+    }
+
+    protected function hydrateContent(): void
+    {
+        if (! $this->content) {
+            return;
+        }
+
+        $url = Str::of(Livewire::originalUrl())
+            ->after(Site::current()->absoluteUrl())
+            ->start('/');
+
+        if (! ($entry = Entry::findByUri($url))) {
+            return;
+        }
+
+        CascadeFacade::withContent($entry);
     }
 }
