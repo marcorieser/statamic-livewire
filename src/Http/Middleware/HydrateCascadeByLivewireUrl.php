5e598375c06ecfd5bf40b6bad3e8878feb7f3f51
Marco Rieser <marco@rieser.dev>
2026-01-16 06:07:00 +0100

    simplify

--- src/Http/Middleware/HydrateCascadeByLivewireUrl.php
+++ src/Http/Middleware/HydrateCascadeByLivewireUrl.php
@@ -5,10 +5,9 @@ namespace MarcoRieser\Livewire\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Request as RequestFacade;
-use Illuminate\Support\Str;
 use Livewire\Livewire;
 use Statamic\Facades\Cascade;
-use Statamic\Facades\Entry;
+use Statamic\Facades\Data;
 use Statamic\Facades\Site;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -40,14 +39,6 @@ class HydrateCascadeByLivewireUrl
 
     protected function hydrateContent(): void
     {
-        $url = Str::of(Livewire::originalUrl())
-            ->after(Site::current()->absoluteUrl())
-            ->start('/');
-
-        if (! ($entry = Entry::findByUri($url, Site::current()))) {
-            return;
-        }
-
-        Cascade::withContent($entry);
+        Cascade::withContent(Data::findByRequestUrl(Livewire::originalUrl()));
     }
 }
