f298976dd4480ee199c0b8137522dc942cf81d6c
Marco Rieser <marco@rieser.dev>
2026-04-02 11:05:18 +0200

    Add the ability to restore the cascade by context

--- src/Attributes/Cascade.php
+++ src/Attributes/Cascade.php
@@ -2,6 +2,7 @@
 
 namespace MarcoRieser\Livewire\Attributes;
 
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Support\Arr;
 use Livewire\Features\SupportAttributes\Attribute as LivewireAttribute;
 use Statamic\Exceptions\CascadeDataNotFoundException;
@@ -10,12 +11,22 @@ use Statamic\Facades\Cascade as CascadeFacade;
 #[\Attribute]
 class Cascade extends LivewireAttribute
 {
-    public function __construct(public array $keys = []) {}
+    public function __construct(
+        public array $keys = [],
+        public bool $context = false,
+        public ?string $contextBy = null,
+    ) {}
 
     public function getCascadeData(): array
     {
         if (! $data = CascadeFacade::toArray()) {
             $data = CascadeFacade::hydrate()->toArray();
+        }
+
+        // TODO[mr]: ensure works in blade (02.04.2026 mr)
+        if ($this->context && $contextIdentifier = $this->resolveContextIdentifier()) {
+            $path = $this->cascadePath(CascadeFacade::get('page'), $contextIdentifier);
+            $data = array_merge($data, CascadeFacade::get($path)?->toArray() ?? []);
         }
 
         if (! $this->keys) {
@@ -36,5 +47,49 @@ class Cascade extends LivewireAttribute
                 return [$key => Arr::get($data, $key, $default)];
             })
             ->all();
+    }
+
+    protected function cascadePath($data, string $value, string $key = 'id', string $currentPath = ''): string
+    {
+        if ($data instanceof Arrayable) {
+            $data = $data->toArray();
+        }
+
+        foreach ($data as $nestedKey => $nestedValue) {
+            if ($nestedKey === $key && $nestedValue === $value) {
+                return $currentPath;
+            }
+
+            if (is_array($nestedValue) || $nestedValue instanceof Arrayable) {
+                $nestedPath = $currentPath === '' ? $nestedKey : $currentPath.'.'.$nestedKey;
+                $result = $this->cascadePath($nestedValue, $value, $key, $nestedPath);
+
+                if ($result !== '') {
+                    return $result;
+                }
+            }
+        }
+
+        return '';
+    }
+
+    protected function resolveContextIdentifier()
+    {
+        $possibleIdentifiers = collect([
+            $this->contextBy,
+            'id',
+            'context',
+            'context_id',
+            'cascade_id',
+            'contextId',
+            'cascadeId',
+        ])
+            ->filter()
+            ->values();
+
+        $attributes = collect($this->getComponent()->getHtmlAttributes());
+        $match = $possibleIdentifiers->intersect($attributes->keys())->first();
+
+        return $attributes[$match] ?? null;
     }
 }
