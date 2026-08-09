8434d3930b4e26b10024436a82ce6a813d146187
Marco Rieser <marco@rieser.dev>
2026-08-09 10:25:06 +0200

    chore: raise phpstan to level max

--- tests/Feature/BootTest.php
+++ tests/Feature/BootTest.php
@@ -7,12 +7,12 @@ use MarcoRieser\Livewire\ServiceProvider;
 use Statamic\Providers\StatamicServiceProvider;
 
 it('registers the addon service provider', function (): void {
-    expect($this->app->getProviders(ServiceProvider::class))->toHaveCount(1);
+    expect(app()->getProviders(ServiceProvider::class))->toHaveCount(1);
 });
 
 it('boots livewire and statamic', function (): void {
-    expect($this->app->getProviders(LivewireServiceProvider::class))->toHaveCount(1)
-        ->and($this->app->getProviders(StatamicServiceProvider::class))->toHaveCount(1);
+    expect(app()->getProviders(LivewireServiceProvider::class))->toHaveCount(1)
+        ->and(app()->getProviders(StatamicServiceProvider::class))->toHaveCount(1);
 });
 
 it('merges the addon config', function (): void {
